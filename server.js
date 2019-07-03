35a1b5e4172fce4eb7fdb535229cdba36bb14fb9
Georgia <gchrisinger@gmail.com>
2019-07-03 11:14:17 -0700

    yep

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = requrie('express');
+var app + express ();
+
+var PORT = process.env.PORT || 3000;
+
+app. get('/',function(req,res){
+    res.send('fu')
+})
+app.listen(PORT, function() {
+    console.log("app listening on port" + PORT);
+})
+
